a298869f4b98ffe60c4b2336e25c23d4d3244d26
Andrey Fabricio <andrey.fas@gmail.com>
2022-02-15 00:12:34 -0300

    Frontend do desafio 2 concluido
    Necessário fazer testes de usabilidade no frontend

---
+++ Senha/src/main/DesafioSenha.java
@@ -0,0 +1,157 @@
+/**
+ * Questão 2 do Desafio de Programação da Academia Capgemini
+ * Esse programa lê uma String e informa qual é o número mínimo de
+ * caracteres que devem ser adicionados para essa String qualquer ser considerada segura
+ * O frontend da aplicação utiliza Java Swing enquanto que o backend é escrito em Java 
+ *  
+ */
+package main;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ * @author Andrey Fabricio
+ * Última atualização: 14/02/2022
+ *
+ */
+public class DesafioSenha extends JFrame implements ActionListener{
+
+	/**
+	 * Descrição do desafio:
+	 * Débora se inscreveu em uma rede social para se manter em contato com seus amigos. 
+	 * A página de cadastro exigia o preenchimento dos campos de nome e senha, porém 
+	 * a senha precisa ser forte. 
+	 * O site considera uma senha forte quando ela satisfaz os seguintes critérios:
+	 * - Possui no mínimo 6 caracteres.
+	 * - Contém no mínimo 1 digito.
+	 * - Contém no mínimo 1 letra em minúsculo.
+	 * - Contém no mínimo 1 letra em maiúsculo.
+	 * - Contém no mínimo 1 caractere especial. Os caracteres especiais são: !@#$%^&*()-+
+	 * Débora digitou uma string aleatória no campo de senha, porém ela não tem certeza se é uma
+	 * senha forte. Para ajudar Débora, construa um algoritmo que informe qual é o número mínimo de
+	 * caracteres que devem ser adicionados para uma string qualquer ser considerada segura.
+	 * 
+	 * 
+	 * e informa qual é o número mínimo de
+* caracteres que devem ser adicionados para essa String qualquer ser considerada segura
+	 */
+	
+	Container container = getContentPane();
+	
+	// Texto explicativo
+	JLabel textoExplicativo = new JLabel("<html>Esse programa lê uma senha e informa se ela é ou não segura.<br/>"
+			+ "O programa pode sugerir uma nova senha (mais segura).<br/>"
+			+ "Essa nova senha será criada com base na senha informada.<br/><br/>"
+			+ "As senhas testadas e sugeridas por esse programa<br/> "
+			+ "não ficarão salvas em nenhum lugar do computador.<br/>"
+			+ "Além disso o programa não conecta com a internet.<br/><br/>"
+			+ "Para começar, digite a senha e depois clique em \"Verificar\"</html>");
+	
+	// Pede ao usuario para digitar a senha
+	JLabel digiteSenha = new JLabel("Digite aqui a senha a ser verificada:");
+	
+	// Input do usuario	
+	JTextField senhaUsuario = new JTextField();
+	
+	// Botão de confirmação
+	JButton btn = new JButton("Verificar");
+
+	// Texto de erro
+	JLabel erro = new JLabel("");
+	
+	// Texto de ajuda
+	JLabel ajuda = new JLabel("<html>Uma senha segura deve conter:<br/>"+
+			"No mínimo 6 caracteres.<br/>" + 
+			"No mínimo 1 número.<br/>" + 
+			"No mínimo 1 letra em minúsculo.<br/>" + 
+			"No mínimo 1 letra em maiúsculo.<br/>" + 
+			"No mínimo 1 caractere especial.<br/>" +
+			"Os caracteres especiais são: !@#$%^&*()-+</html>");
+	
+	// Caixa de texto
+    JTextArea sugestoes = new JTextArea();
+	
+	public DesafioSenha() {
+		
+		container.setLayout(null);
+    	
+    	// Tamanho e posição dos componentes
+    	textoExplicativo.setBounds(20,10,400,150);
+    	digiteSenha.setBounds(20,140,250,70);
+    	senhaUsuario.setBounds(20, 185, 200, 20);
+    	btn.setBounds(220, 185, 85, 19);
+    	erro.setBounds(20, 200, 400, 20);
+    	sugestoes.setBounds(20, 220, 400, 150);
+    	ajuda.setBounds(20, 360, 400, 150);
+    	
+    	sugestoes.setLineWrap(true);
+    	sugestoes.setEditable(false);
+    	sugestoes.setVisible(true);    	
+    	sugestoes.setFont(new Font("Serif",Font.BOLD,15));// Muda o tamanho e a fonte do texto
+    	
+    	erro.setForeground(new java.awt.Color(255,0,0)); // Muda para vermelho a cor do erro
+    	
+    	// Coloca os componentes na janela
+    	container.add(textoExplicativo);
+    	container.add(digiteSenha);
+    	container.add(senhaUsuario);
+    	container.add(btn);
+    	container.add(erro);
+    	container.add(sugestoes);
+    	container.add(ajuda);
+    	
+    	// Adiciona um listener para o botão
+    	btn.addActionListener(this);
+    	
+    	// Remove a janela de seleção ao redor do texto do botão
+    	btn.setFocusPainted(false);
+    	
+    	// Cria um listener para a tecla "Enter"
+    	senhaUsuario.addActionListener(this); 
+    	
+    	// Adiciona borda
+    	sugestoes.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1));
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		// Se o botão for clicado ou o usuário apertar a tecla enter
+    	if (e.getSource() == btn || e.getSource() == senhaUsuario) {
+        	erro.setText(""); // Reseta o erro
+        	testaSenha(); // Cria a escada
+        	senhaUsuario.setText(""); // Reseta o input do usuário
+        }
+		
+	}
+	
+	private void testaSenha() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String[] args) {
+		
+		DesafioSenha frame=new DesafioSenha(); // Cria um novo frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Encerra o programa ao fechar a janela
+        frame.setTitle("Desafio Capgemini - Senha"); // Define o titulo
+        frame.pack(); // Minimiza os erros de tamanho do layout
+        frame.setSize(445, 540); // Define o tamanho do frame
+        frame.setLocationRelativeTo(null); // Centraliza o frame
+        frame.setResizable(false);
+        frame.setVisible(true); // Torna o frame visivel
+		
+	}
+
+}
